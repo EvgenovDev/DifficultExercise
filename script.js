11891d152fcc4c54c4ccd2ce3c94fee682646e03
Roman Evgenov <Roman Evgenov>
2021-10-04 22:06:39 +0300

    Сделана вся работа

--- script.js
+++ script.js
@@ -21,37 +21,48 @@
 
 let lang = prompt("Введите язык на котором хотите увидеть названия дней недели", "Рус-для русского / Англ - для английского");
 
-
+// Массив для решений А и Б,для решения С внизу есть другой массив
 let days = [
 	["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресение"],
 	["Sunday","Monday","Tuesday","Wendsday","Thursday","Friday","Saturday"]
 ];
 
-// if (lang === "Рус") {
-// 	days[0].forEach((element) => {
-// 		console.log(element);
-// 	});
-// 	} else if (lang ==="Англ") {
-// 		days[1].forEach((element) => {
-// 			console.log(element);
-// 		});
-// 	} else if (lang === "") {
-// 		console.log("Вы ничего не ввели. Обновите страницу и попробуйте еще раз");
-// 	} else {
-// 		console.log("Вы ввели не то слово, либо ввели с маленькой буквы, либо нажали отмена. Обновите страницу и попробуйте еще раз");
-// 	}
+// Вариант - А
+if (lang === "Рус") {
+	days[0].forEach((element) => {
+		console.log(element);
+	});
+	} else if (lang ==="Англ") {
+		days[1].forEach((element) => {
+			console.log(element);
+		});
+	} else if (lang === "") {
+		console.log("Вы ничего не ввели. Обновите страницу и попробуйте еще раз");
+	} else {
+		console.log("Вы ввели не то слово, либо ввели с маленькой буквы, либо нажали отмена. Обновите страницу и попробуйте еще раз");
+	}
 
-switch (lang) {
-	case "Рус":
-		days[0].forEach((elem) => {
-			console.log(elem);
-		});
-		break;
-	case "Англ": 
-		days[0].forEach((elem) => {
-			console.log(elem);
-		});
-		break;
-	default:
-		console.log("Вы ввели не то слово или ничего не ввели,либо ввели с маленькой буквы, либо нажали отмена. Обновите страницу и попробуйте еще раз");
-}
+//Вариант - Б
+// switch (lang) {
+// 	case "Рус":
+// 		days[0].forEach((elem) => {
+// 			console.log(elem);
+// 		});
+// 		break;
+// 	case "Англ": 
+// 		days[0].forEach((elem) => {
+// 			console.log(elem);
+// 		});
+// 		break;
+// 	default:
+// 		console.log("Вы ввели не то слово или ничего не ввели,либо ввели с маленькой буквы, либо нажали отмена. Обновите страницу и попробуйте еще раз");
+// }
+
+// Вариант-С
+// let days1 = [];
+// days1["Рус"] = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресение"];
+// days1["Англ"] = ["Sunday","Monday","Tuesday","Wendsday","Thursday","Friday","Saturday"];
+// console.log(days1[lang]);
+let namePerson = prompt("Введите полное имя с большой буквы", "Роман");
+
+namePerson = namePerson === "Артем" ? console.log("Директор") : namePerson === "Александр" ? console.log("Преподователь") : console.log("Студент");
