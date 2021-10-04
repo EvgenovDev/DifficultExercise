d259799417aa3ab093253453149f5a3e93f29483
Roman Evgenov <Roman Evgenov>
2021-10-04 21:26:32 +0300

    Сделана первая часть работы на конструкции if else

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-let num = 266219;
-//Создаем массив делая каждую цифру элементом массива из значения переменной num
-let numStr = num.toString();
-let numArray = numStr.split("");
-//Создаем переменную для записи итога перемножения цифр и записываем в нее самую первую цифру массива
-let multipliedNum = +numArray[0];
-let cubeRootNum = 0;
+"use strict";
+// let num = 266219;
+// //Создаем массив делая каждую цифру элементом массива из значения переменной num
+// let numStr = num.toString();
+// let numArray = numStr.split("");
+// //Создаем переменную для записи итога перемножения цифр и записываем в нее самую первую цифру массива
+// let multipliedNum = +numArray[0];
+// let cubeRootNum = 0;
 
-for(let i = 1; i < numArray.length; i++){
-	multipliedNum *= +numArray [i];
-}
-//Возводим в куб наше перемноженное число 
-cubeRootNum =  multipliedNum**3;
+// for(let i = 1; i < numArray.length; i++){
+// 	multipliedNum *= +numArray [i];
+// }
+// //Возводим в куб наше перемноженное число 
+// cubeRootNum =  multipliedNum**3;
 
-console.log("Результат перемножения цифр в числе " + num + " -> " + multipliedNum);
+// console.log("Результат перемножения цифр в числе " + num + " -> " + multipliedNum);
 
-console.log ("Возводим полученное число сверху в куб -> " + multipliedNum + "^3" + " = "  + cubeRootNum);
+// console.log ("Возводим полученное число сверху в куб -> " + multipliedNum + "^3" + " = "  + cubeRootNum);
 
-console.log ("Первые две цифры от числа " + cubeRootNum + " -> " + (cubeRootNum.toString()).substring(0,2));
+// console.log ("Первые две цифры от числа " + cubeRootNum + " -> " + (cubeRootNum.toString()).substring(0,2));
+
+let lang = prompt("Введите язык на котором хотите увидеть названия дней недели", "Рус-для русского / Англ - для английского");
+
+
+let days = [
+	["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресение"],
+	["Sunday","Monday","Tuesday","Wendsday","Thursday","Friday","Saturday"]
+];
+
+if (lang === "Рус") {
+	days[0].forEach((element) => {
+		console.log(element);
+	});
+	} else if (lang ==="Англ") {
+		days[1].forEach((element) => {
+			console.log(element);
+		});
+	} else if (lang === "") {
+		console.log("Вы ничего не ввели. Обновите страницу и попробуйте еще раз");
+	} else {
+		console.log("Вы ввели не то слово, либо ввели с маленькой буквы, либо нажали отмена. Обновите страницу и попробуйте еще раз");
+	}
