d7edc1be7a09ecfa3e996f2879d4628abf4fa9fe
Roman Evgenov <Roman Evgenov>
2021-10-09 17:36:05 +0300

    Готовое задание

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение"];
+let today = new Date ();
+
+week.forEach((elem, index) => {
+	if (elem === "Суббота" || elem === "Воскресение") {
+		elem = elem.italics();
+		if (index === today.getDay()) {
+			elem = elem = elem.italics().bold();
+		}
+	} else if (index === today.getDay()) {
+		elem = elem.bold();
+	}
+	console.log(elem);
+});
